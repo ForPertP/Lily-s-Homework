407f86b38e4544852245b3dfafd686ce7e20ae55
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-09-18 07:17:35 +0900

    Refactor rtrim function to remove duplicate code
    Removed redundant implementation of rtrim function.

--- Lily-s-Homework.cpp
+++ Lily-s-Homework.cpp
@@ -95,14 +95,6 @@ string ltrim(const string &str)
     return s;
 }
 
-string rtrim(const string &str) {
-    string s(str);
-    s.erase(
-        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
-        };
-    return s;
-}
-
 string rtrim(const string &str)
 {
     string s(str);
