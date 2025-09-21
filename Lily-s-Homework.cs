d7bca45d946899453f699050b711dd7833285b28
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-09-22 07:20:30 +0900

    Update Lily-s-Homework.cs

--- Lily-s-Homework.cs
+++ Lily-s-Homework.cs
@@ -24,3 +24,24 @@ class Result
      */
 
 }
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.lilysHomework(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
