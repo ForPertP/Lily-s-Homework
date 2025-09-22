014529e1fde36c0966b144a72f644c3310e3e68c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-09-23 07:20:57 +0900

    Update Lily-s-Homework.cs

--- Lily-s-Homework.cs
+++ Lily-s-Homework.cs
@@ -12,7 +12,6 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-
 class Result
 {
 
@@ -23,8 +22,56 @@ class Result
      * The function accepts INTEGER_ARRAY arr as parameter.
      */
 
-}
+   static int CountSwaps(List<int> arr, List<int> sorted)
+    {
+        int n = arr.Count;
+        Dictionary<int, int> pos = new Dictionary<int, int>();
 
+        for (int i = 0; i < n; i++)
+        {
+            pos[sorted[i]] = i;
+        }
+
+        bool[] visited = new bool[n];
+        int swaps = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (visited[i] || pos[arr[i]] == i)
+                continue;
+
+            int cycleSize = 0;
+            int j = i;
+
+            while (!visited[j])
+            {
+                visited[j] = true;
+                j = pos[arr[j]];
+                cycleSize++;
+            }
+
+            if (cycleSize > 1)
+                swaps += (cycleSize - 1);
+        }
+
+        return swaps;
+    }
+
+
+    public static int lilysHomework(List<int> arr)
+    {
+        List<int> asc = new List<int>(arr);
+        List<int> desc = new List<int>(arr);
+
+        asc.Sort();
+        desc.Sort((a, b) => b.CompareTo(a));
+
+        int ascSwaps = CountSwaps(arr, asc);
+        int descSwaps = CountSwaps(arr, desc);
+
+        return Math.Min(ascSwaps, descSwaps);
+    }
+}
 
 
 class Solution
